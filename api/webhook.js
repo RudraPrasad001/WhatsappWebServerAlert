06ef1c0b2a5036a36786c0d04b846ee8d3eba5a6
Rudra Prasad <rudraprasad.email@gmail.com>
2026-04-15 18:35:21 +0530

    chore : Update help message

--- api/webhook.js
+++ api/webhook.js
@@ -81,6 +81,7 @@ export default async function handler(req, res) {
     const helpMessage = `🤖 *Web-Server Bot Commands*\n\n` +
         `🟢 *!status* - Check if the server is UP or DOWN and see the current capacity.\n` +
         `🎮 *!players* - Get a live list of everyone currently playing on the server.\n` +
+        `🏠 *!ip* - Get the IP Address of the server.\n` +
         `ℹ️ *!help* - Show this command menu.`;
 
     const greenApiUrl = `https://api.green-api.com/waInstance${process.env.GREEN_API_ID}/sendMessage/${process.env.GREEN_API_TOKEN}`;
